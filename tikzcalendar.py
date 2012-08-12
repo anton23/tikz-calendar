ff952a309a2f481060f3659b1400f2aeb52da965
Anton Stefanek <tonkos@gmail.com>
2012-08-12 22:55:45 +0100

    Fixed example pdf's

--- tikzcalendar.py
+++ tikzcalendar.py
@@ -1,49 +1,38 @@
-try:
-  from xml.etree import ElementTree
-except ImportError:
-  from elementtree import ElementTree
-
-from lxml import etree
-
+import atom.data
+import codecs
+from collections import defaultdict
+import datetime
 import gdata.calendar.data
 import gdata.calendar.client
 import gdata.acl.data
-import atom.data
 import iso8601
 from jinja2 import Environment
 from jinja2 import Template
 from jinja2 import FileSystemLoader
-import codecs
-import time
-import datetime
-from collections import defaultdict
-import UserDict
+from lxml import etree
+import os
 import sys
+import time
+import UserDict
 
-COLOUR_MAP = 'colourMap'
-COLOUR = 'colour'
 LOCATION = 'location'
-CALENDAR_MAIN = 'gcalatex'
-GOOGLE_EVENTS = 'googleEvents'
-FEED_URI = 'feed_uri'
-COLUMN_MAP = 'columnMap'
-COLUMN_MAP_ID = 'columnMap'
+CALENDAR_MAIN = 'TikZCalendar'
 XPOS = 'xpos'
 WIDTH = 'width'
 ID = 'id'
 TITLE = 'title'
 AUTHORS = 'authors'
 START = 'start'
+END = 'end'
+OUTPUT = 'output'
 
-COLUMN_EXCEPTION = 'column_exception'
-TALK_EXCEPTION = 'talk_exception'
-NEW_AUTHORS = 'new_authors'
 
-MIN_DUR = 'min_dur'
-MAX_LENGTH = 'max_length'
+def to_field_name(name):
+    return name[0].lower() + name[1:]
 
 class EventsProvider(object):
     pass
+
 
 class Event(object):
     
@@ -55,12 +44,20 @@ class Event(object):
         self.width = None
         self.column = None
 
+
 class SessionEvent(Event):
     def __init__(self, title, start, end, where, talks):
         super(SessionEvent, self).__init__(title, start, end, where)
         self.talks = talks    
 
-class GoogleEventsProvider(EventsProvider):
+
+class GoogleEvents(EventsProvider):
+    NAME = 'GoogleEvents'
+    FEED_URI = 'feed_uri'
+
+    @classmethod
+    def create(cls, tree):
+        return GoogleEvents(tree.find(cls.NAME).get(cls.FEED_URI))
 
     @classmethod
     def tex_escape(cls, s):
@@ -76,13 +73,13 @@ class GoogleEventsProvider(EventsProvider):
         
     @classmethod
     def get_event(cls, e): 
-        title=GoogleEventsProvider.tex_escape(e.title.text)
-        start=GoogleEventsProvider.parse_date(e.when[0].start)
-        end=GoogleEventsProvider.parse_date(e.when[0].end)
+        title=GoogleEvents.tex_escape(e.title.text)
+        start=GoogleEvents.parse_date(e.when[0].start)
+        end=GoogleEvents.parse_date(e.when[0].end)
         where=e.where[0].value     
         if e.content.text:
             # This is a session event, we find the individual talks
-            talksS = [map(GoogleEventsProvider.tex_escape, t.split('\n'))
+            talksS = [map(GoogleEvents.tex_escape, t.split('\n'))
                 for t in e.content.text.split('\n\n')]
             # Check if the description describes talks
             if not filter(lambda x:len(x) != 2, talksS):
@@ -104,15 +101,14 @@ class GoogleEventsProvider(EventsProvider):
          self.feed_uri = feed_uri.replace('basic', 'full?max-results=999999')
          calendar_client = gdata.calendar.client.CalendarClient()
          feed = calendar_client.GetCalendarEventFeed(uri=self.feed_uri)
-         events = [GoogleEventsProvider.get_event(f) for f in feed.entry]
+         events = [GoogleEvents.get_event(f) for f in feed.entry]
+         print "  Found", len(events), "events"
          self.events = sorted(events, key=lambda e:(e.start, e.title))
-         
-         
+                
     def get_events(self):
         return self.events
          
                   
-
 class XMLEventsProvider(EventsProvider):
     pass
 
@@ -132,7 +128,18 @@ class EventException(object):
             return True
         return False
 
+
 class ColumnException(EventException):
+
+    NAME = 'ColumnException'
+    TITLE = 'title'
+    XPOS = 'xpos'
+    WIDTH = 'width'
+
+    @classmethod
+    def create(cls, e):
+        return ColumnException(e.get(cls.TITLE),
+                                (e.get(cls.XPOS), e.get(cls.WIDTH)))
 
     def __init__(self, title, new_layout):
         self.title = title
@@ -145,8 +152,21 @@ class ColumnException(EventException):
         e.width = float(self.new_layout[1])
         e.column = float(self.new_layout[0])
 
+    def __str__(self):
+        return "For event with title \""+ self.title + \
+               "\" change column to " + self.new_layout[0] + \
+               " and width to " + self.new_layout[1]
+    
 
 class TalkException(EventException):
+
+    NAME = 'TalkException'
+    TITLE = 'title'
+    NEW_AUTHORS = 'new_authors'
+
+    @classmethod
+    def create(cls, e):
+        return TalkException(e.get(cls.TITLE), e.get(cls.NEW_AUTHORS))
 
     def __init__(self, title, new_authors):
         self.title = title
@@ -167,123 +187,162 @@ class TalkException(EventException):
     def __str__(self):
         return "Change authors in " + self.title + " to " + self.new_authors
 
+
 class ColumnMap(object):
+
+    NAME = 'ColumnMap'
+    MIN_DUR = 'min_dur'
+    MAX_LENGTH = 'max_length'
+    TITLE = 'title'
+    COLUMN = 'column'
+    COLOUR = 'colour'
     
     def __init__(self, cm):
+        print "Processing", ColumnMap.NAME, "with ID", cm.get(ID)
         self.column_map = dict(
              [(s.get(LOCATION),(s.get(XPOS), s.get(WIDTH))) 
                 for s in cm.findall('set')]) 
         self.exceptions = []
-        for e in cm.findall(COLUMN_EXCEPTION):
-            self.exceptions.append(
-                ColumnException(e.get(TITLE),
-                                (e.get(XPOS), e.get(WIDTH))))
-        for e in cm.findall(TALK_EXCEPTION):
-            transformation = TalkException(e.get(TITLE), e.get(NEW_AUTHORS))
-            print "Talk transformation", str(transformation)
+        for e in cm.findall(ColumnException.NAME):
+            exp = ColumnException.create(e)
+            self.exceptions.append(exp)
+            print "  Column exception:", str(exp)
+        for e in cm.findall(TalkException.NAME):
+            transformation = TalkException.create(e)
+            print "  Talk transformation:", str(transformation)
             self.exceptions.append(transformation)
-        self.min_dur = float(cm.get(MIN_DUR) or '0')
-        if cm.get(MAX_LENGTH):
-            self.max_length = int(cm.get(MAX_LENGTH))
-            print MAX_LENGTH, "is", self.max_length
+        self.min_dur = float(cm.get(ColumnMap.MIN_DUR) or '0')
+        if cm.get(ColumnMap.MAX_LENGTH):
+            self.max_length = int(cm.get(ColumnMap.MAX_LENGTH))
+            print "  " + ColumnMap.MAX_LENGTH, "is", self.max_length
         else:
             self.max_length = None
+
+        self.column_titles=[]
+        for ct in cm.findall(ColumnMap.COLUMN):
+            self.column_titles.append({
+                    ColumnMap.TITLE:ct.get(ColumnMap.TITLE),
+                    XPOS:float(ct.get(XPOS)),
+                    WIDTH:float(ct.get(WIDTH)),
+                    ColumnMap.COLOUR:ct.get(ColumnMap.COLOUR)
+                }
+            )
+            
                            
-        
     def get(self, location):
         return self.column_map.get(location)
     
 
-
-
 class ColourMap(object):
-    
+   
+    NAME = 'ColourMap'
+    COLOUR = 'colour'
+    LOCATION = 'location'
+
     @classmethod
     def create_map(cls, e):
         if e is not None:
-            return ColourMap(dict([(s.get(LOCATION), s.get(COLOUR)) for s in e]))
+            return ColourMap(dict([(s.get(ColourMap.LOCATION), s.get(ColourMap.COLOUR)) for s in e]))
         # Returns default colour map
         else:
+            print "No", ColourMap.NAME, "defined!"
             return dict()
         
     def __init__(self, d):
-        self.d = d
-        
+        self.d = d      
         
     def get(self, key):       
         return self.d.get(key, 'color1')
-        
-
-
-def get_date(e):
-    return datetime.date(year=int(e.get('year')),
-        month=int(e.get('month')),day=int(e.get('day')))
-
-def get_time(e):
-    return datetime.time(hour=int(e.get('hour') or '0'),
-        minute=int(e.get('minute') or '0'),second=int(e.get('second') or '0'))
 
 
 class CalendarEnvironment(object):
     
     def __init__(self, input_file):
         self.input_file = input_file
+        self.env = Environment(loader=FileSystemLoader('templates'))
         
     def collect_column_maps(self):
         self.column_maps = {} 
-        for map in self.tree.findall(COLUMN_MAP):
+        for map in self.tree.findall(ColumnMap.NAME):
             self.column_maps[map.get(ID)] = ColumnMap(map)
+        print "Found", len(self.column_maps), "column maps"
             
     def parse_input(self):
         fp = codecs.open(self.input_file, mode='r', encoding="utf-8")
         # Gets the main xml tree
         self.tree = etree.parse(fp)
         # Creates the colour map
-        self.colour_map = ColourMap.create_map(self.tree.find(COLOUR_MAP))
+        self.colour_map = ColourMap.create_map(self.tree.find(ColourMap.NAME))
         # Retreives events from Google if specified
-        if self.tree.find(GOOGLE_EVENTS) is not None:   
+        if self.tree.find(GoogleEvents.NAME) is not None:   
             print "Found google events"        
-            self.events_provider = GoogleEventsProvider(self.tree.find(GOOGLE_EVENTS).get(FEED_URI))
+            self.events_provider = GoogleEvents.create(self.tree)
         # Otherwise looks for local events
         # TODO add this
         
         self.collect_column_maps()
                 
-        for calendar in self.tree.findall('calendar'):
-            Calendar(calendar, self.events_provider, self.column_maps, self.colour_map)        
+        for calendar in self.tree.findall(Calendar.NAME):
+            Calendar(calendar, self.events_provider, self.column_maps, self.colour_map, self.env)        
 
             
-
 class Calendar(object):
-    
-    def __init__(self, calendar, events_provider, column_maps, colour_map):        
+
+    NAME = 'Calendar'
+    COLUMN_MAP_NAME = to_field_name(ColumnMap.NAME)
+    MIN_TIME = 'minTime'
+    MAX_TIME = 'maxTime'
+    SESSION_TITLES = 'sessionTitles'
+
+    @classmethod
+    def get_time(cls, e):
+        return datetime.time(hour=int(e.get('hour') or '0'),
+            minute=int(e.get('minute') or '0'),second=int(e.get('second') or '0'))
+
+    @classmethod
+    def get_date(cls, e):
+        return datetime.date(year=int(e.get('year')),
+            month=int(e.get('month')),day=int(e.get('day')))
+
+
+    def __init__(self, calendar, events_provider, column_maps, colour_map, env):        
+        self.env = env
         self.max_hour = 20
         self.min_hour = 7.75
         self.min_dur = None
         self.max_length = None
     
-        start_date = get_date(calendar.find('start'))
-        end_date = get_date(calendar.find('end'))
-        output = calendar.get('output')
+        start_date = Calendar.get_date(calendar.find(START))
+        end_date = Calendar.get_date(calendar.find(END))
+        output = calendar.get(OUTPUT)
         
         print "Rendering calendar from", start_date, "to", end_date, \
                 "into", output           
 
-
-        column_map = column_maps.get(calendar.get(COLUMN_MAP_ID))
+        column_map = column_maps.get(calendar.get(Calendar.COLUMN_MAP_NAME))
+        if column_map is None:
+            print "Warning, calendar without a column map"
 
         self.daytitle = True
         if calendar.get('daytitle') == 'False':
             self.daytitle = False
 
+        self.session_titles = True
+        if calendar.get(Calendar.SESSION_TITLES) == 'False':
+            self.session_titles = False
 
-        self.min_time = get_time(calendar.find('minTime'))
+
+
+        self.min_time = Calendar.get_time(calendar.find(Calendar.MIN_TIME))
         self.min_hour = self.min_time.hour + self.min_time.minute/60.0
-        self.max_time = get_time(calendar.find('maxTime'))
+        self.max_time = Calendar.get_time(calendar.find(Calendar.MAX_TIME))
         self.max_hour = self.max_time.hour + self.max_time.minute/60.0
+
     
-        self.processCalendarDays(events_provider.get_events(), [(start_date, end_date, output)],
-            colour_map, column_map=column_map)        
+        self.process_calendar_days(events_provider.get_events(), [(start_date, end_date, output)],
+            colour_map, column_map=column_map,
+            session_titles=self.session_titles,
+            column_titles=column_map.column_titles)        
 
 
     def assign_columns_to_rooms(self, days, column_map, colour_map):
@@ -299,13 +358,10 @@ class Calendar(object):
                     exception.apply_to_event(e)
     
     
-    def processCalendarDays(self, events, day_ranges, colour_map, column_map=None, session_titles=True, session_bars=True,
+    def process_calendar_days(self, events, day_ranges, colour_map, column_map=None, session_titles=True, session_bars=True,
         column_titles=None, column_colors=None,daytitle = True, template='calendar.temp'):
         
-        
         for (start_day, end_day, filename) in day_ranges:
-            
-            
             days = defaultdict(list)
             for e in events:
                 day = e.start.date()
@@ -313,9 +369,6 @@ class Calendar(object):
                     days[day].append(e)
                     
             days = sorted(days.items(), key=lambda t:t[0])
-           
-    
-            
     
             if column_map:
                 # remove rooms not in the map first
@@ -328,26 +381,25 @@ class Calendar(object):
                         ))
                 , days)
                 self.assign_columns_to_rooms(days, column_map, colour_map)
+                print "  Total number of events:", sum([len(d[1]) for d in days])
                 
-            s = env.get_template(template).render(
+            s = self.env.get_template(template).render(
                 days=days,min_hour=self.min_hour, max_hour=self.max_hour, session_titles=session_titles,
                 column_titles = column_titles,
                 column_colors=column_colors,daytitle=self.daytitle,
                 min_dur = column_map.min_dur, max_length = column_map.max_length,
                 session_bars=session_bars)
+            directory = '/'.join(filename.split('/')[:-1])
+            if not os.path.exists(directory):
+                os.makedirs(directory)
             f = codecs.open(filename, 'w',encoding='utf-8')
             f.write(s)
             f.close()
-    
 
 
 
-def parseDate(s):
-    return datetime.datetime.strptime(s.split('T')[0],'%Y-%m-%d').date()
+def main():
+    inputXML = sys.argv[1]
+    CalendarEnvironment(inputXML).parse_input()
 
-env = Environment(loader=FileSystemLoader('templates'))
-
-
-inputXML = sys.argv[1]
-
-CalendarEnvironment(inputXML).parse_input()
+if __name__ == '__main__':main()
